5c0a8015df1b671b49670c99a40a346f3f688911
Vinitharam16 <vinithabegp@gmail.com>
2024-05-09 22:51:54 +0530

    second commit

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDatabase() {
     const dbUrl = `${process.env.MONGODB_URI_LOCAL}${process.env.MONGODB_DATABASE_NAME}`
-    mongoose.connect(dbUrl)
+    mongoose.connect(dbUrl, { useNewUrlParser: true })
         .then((response) => console.log("Database connected successfully"))
         .catch((err) => console.log(err));
 
